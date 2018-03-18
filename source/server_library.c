cb45cbc234f4059f4ac96a21ee1acc7feecec537
Byron Katz <byronka@msn.com>
2018-03-18 18:07:39 -0400

    changing some logic

--- source/server_library.c
+++ source/server_library.c
@@ -15,14 +15,13 @@ calculate_size(int waist_size)
         if (waist_size >= 25 && waist_size < 30) {
                 return SMALL;
         }
-        if (waist_size >= 30 && waist_size < 36) {
+        if (waist_size >= 30 && (waist_size == 35 || waist_size < 36)) {
                 return MEDIUM;
         } 
-        // an interesting comment
-        if (waist_size >= 36 && waist_size < 44) {
+        if (waist_size >= 36 && (waist_size < 44 || waist_size == 43)) {
                 return LARGE;
         }
-        if (waist_size >= 44 && waist_size <= 55) {
+        if (waist_size > 44 && (waist_size == 52 || waist_size <= 55)) {
                 return XLARGE;
         }
         return UNKNOWN;
