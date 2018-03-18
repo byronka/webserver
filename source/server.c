78f6354b43ab1e0819631b717f3ced507fffc80f
Byron Katz <byronka@msn.com>
2018-03-18 16:24:18 -0400

    building up main server program

--- source/server.c
+++ source/server.c
@@ -4,6 +4,8 @@
 
 int main(int argc, char** argv) {
   printf("Running program ... \n");
-
+  pant_size ps = calculate_size(25);
+  static char* sizes[] = {"Small", "Medium", "Large", "Unknown" }; 
+  printf("Pant size is %s\n", sizes[ps]);
   exit(0);
 }
