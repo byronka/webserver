d9b4ff0944899dd7520ac23e8547d0950ac33326
Byron Katz <byronka@msn.com>
2018-03-21 17:54:40 -0400

    more tests and now using test compile script

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -5,7 +5,7 @@ SCRIPT=$(readlink -f "$0")
 CURRENT_DIR=$(dirname "$SCRIPT")
 source $CURRENT_DIR/script_standards.sh
 
-$CURRENT_DIR/compile_source.sh
+$CURRENT_DIR/compile_source_for_test.sh
 
 echo starting to link files for testing...
 
@@ -16,15 +16,23 @@ cc \
   -o $WEBSERVER_DEVEL_BUILD/business_tests 
 echo business tests linked.
 
-echo linking files for server testing...
+echo linking files for mocked server testing...
 cc \
   $WEBSERVER_DEVEL_BUILD/server_library.o \
   $WEBSERVER_DEVEL_BUILD/server_library_test.o \
   -o $WEBSERVER_DEVEL_BUILD/server_tests 
 echo server tests linked.
 
+echo linking files for integrated server testing...
+cc \
+  $WEBSERVER_DEVEL_BUILD/server_library.o \
+  $WEBSERVER_DEVEL_BUILD/server_library_test_integration.o \
+  -o $WEBSERVER_DEVEL_BUILD/server_tests_integrated
+echo server tests linked.
+
 echo running tests...
 $WEBSERVER_DEVEL_BUILD/server_tests
+$WEBSERVER_DEVEL_BUILD/server_tests_integrated
 $WEBSERVER_DEVEL_BUILD/business_tests
 echo work finished.
 
