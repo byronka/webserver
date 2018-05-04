3ffb66886616841ac355ca9038fe1d5b46ba6e1c
Byron Katz <byron@renomad.com>
2018-05-03 21:04:58 -0400

    adding some transparency for deleting the build dir

--- scripts/delete_build.sh
+++ scripts/delete_build.sh
@@ -16,5 +16,8 @@ source $CURRENT_DIR/script_standards.sh
 # remove the top-level build directory
 if [ -d "$WEBSERVER_DEVEL_BUILD" ]; then
   rm -fr $WEBSERVER_DEVEL_BUILD
+  echo "deleted $WEBSERVER_DEVEL_BUILD"
+  exit
 fi
+echo "no directory exists at $WEBSERVER_DEVEL_BUILD"
 
